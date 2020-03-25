9397c6407e496f8816b8dbccc4359854078614e1
jaybarria(L340) <jaybarria@gmail.com>
2020-03-25 08:28:15 +0800

    add loading indicator for image feed

--- app/src/main/java/com/jairrab/myapp/view/home/ImageViewHolder.kt
+++ app/src/main/java/com/jairrab/myapp/view/home/ImageViewHolder.kt
@@ -1,12 +1,20 @@
 package com.jairrab.myapp.view.home
 
+import android.graphics.drawable.Drawable
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import androidx.core.content.ContextCompat.getColor
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.jairrab.myapp.databinding.ViewFeedItemBinding
 import com.jairrab.myapp.models.Post
 import com.jairrab.myapp.models.UserAction
 import com.jairrab.myapp.repo.RemoteRepo
+import com.jairrab.myapp.utils.showView
 
 class ImageViewHolder(
     private val colors: Array<Int>,
@@ -32,9 +40,40 @@ class ImageViewHolder(
         binding.userTv.text = item.user
         val color = getColor(context, colors[getStringNum(item.user!!)])
         binding.userTv.setBackgroundColor(color)
+        loadImage(item)
+    }
+
+    private suspend fun loadImage(item: Post) {
+        binding.progressBar.showView(true)
+        binding.imageView.visibility = INVISIBLE
         val image = remoteRepo.getImage(item)
         Glide.with(binding.imageView.context)
             .load(image)
+            .addListener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.progressBar.showView(false)
+                    binding.imageView.visibility = VISIBLE
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.progressBar.showView(false)
+                    binding.imageView.visibility = VISIBLE
+                    return false
+                }
+
+            })
             .into(binding.imageView)
     }
 
--- app/src/main/java/com/jairrab/myapp/view/user/ImageViewHolder.kt
+++ app/src/main/java/com/jairrab/myapp/view/user/ImageViewHolder.kt
@@ -1,8 +1,15 @@
 package com.jairrab.myapp.view.user
 
+import android.graphics.drawable.Drawable
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import androidx.core.content.ContextCompat.getColor
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.jairrab.myapp.R
 import com.jairrab.myapp.databinding.ViewUserFeedItemBinding
 import com.jairrab.myapp.models.Post
@@ -41,9 +48,40 @@ class ImageViewHolder(
         binding.deleteIv.showView(isLoggedInUser)
         binding.publicIv.showView(isLoggedInUser)
         if (isLoggedInUser) updatePublicIcon(item)
+        loadImage(item)
+    }
+
+    private suspend fun loadImage(item: Post) {
+        binding.progressBar.showView(true)
+        binding.imageView.visibility = INVISIBLE
         val image = remoteRepo.getImage(item)
         Glide.with(binding.imageView.context)
             .load(image)
+            .addListener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.progressBar.showView(false)
+                    binding.imageView.visibility = VISIBLE
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    binding.progressBar.showView(false)
+                    binding.imageView.visibility = VISIBLE
+                    return false
+                }
+
+            })
             .into(binding.imageView)
     }
 
